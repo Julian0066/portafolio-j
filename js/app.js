03e7e6356f62ee8b70fae28d857538ab28b4d458
Julian <jr7julian@gmail.com>
2026-07-20 18:30:07 -0500

    Actualización completa del portafolio

---
+++ js/app.js
@@ -0,0 +1,54 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff" 
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+        },
+        "opacity": {
+            "value": 0.2,
+            "random": false,
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+        },
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": false,
+            },
+            "onclick": {
+                "enable": false,
+            },
+            "resize": true,
+        },
+    },
+    "retina_detect": true,
+});
